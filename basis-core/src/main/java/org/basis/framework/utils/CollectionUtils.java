e60fae583404c8d31d7ef0836e083c41a68ccf02
1690147900@qq.com <1690147900@qq.com>
2025-07-16 18:08:38 +0800

    collection 工具类完善

--- basis-core/src/main/java/org/basis/framework/utils/CollectionUtils.java
+++ basis-core/src/main/java/org/basis/framework/utils/CollectionUtils.java
@@ -1,7 +1,13 @@
 package org.basis.framework.utils;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ArrayUtil;
+
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 
@@ -49,4 +55,24 @@ public class CollectionUtils {
         return asList(createList, updateList, deleteList);
     }
 
+    public static <T, U> List<U> convertList(T[] from, Function<T, U> func) {
+        if (ArrayUtil.isEmpty(from)) {
+            return new ArrayList<>();
+        }
+        return convertList(Arrays.asList(from), func);
+    }
+
+    public static <T, U> List<U> convertList(Collection<T> from, Function<T, U> func) {
+        if (CollUtil.isEmpty(from)) {
+            return new ArrayList<>();
+        }
+        return from.stream().map(func).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    public static <T, U> List<U> convertList(Collection<T> from, Function<T, U> func, Predicate<T> filter) {
+        if (CollUtil.isEmpty(from)) {
+            return new ArrayList<>();
+        }
+        return from.stream().filter(filter).map(func).filter(Objects::nonNull).collect(Collectors.toList());
+    }
 }
